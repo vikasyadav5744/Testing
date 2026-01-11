b63504cc125aef92843bd09f88fc054b14a30099
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-01-11 10:55:22 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,25 +1,30 @@
-import streamlit as st
 import pandas as pd
 import numpy as np
 
-rest = {'calloi':[2500,6500,7600,8700,10000,6700,6200,7700,5500],
-       'callvol':[6000,6700,4300,6500,6600,6700,15000,7600,7800],
-        'strike':[25000,25050,25100,25150,25200,25250,25300,25350,25400],
-         'spot':[25187,25187,25187,25187,25187,25187,25187,25187,25187],
-       }
-data= pd.DataFrame(rest)
-data['cemax']=data['calloi'].max()
-data['volcemax']=data['calloi'].max()
-data['volcemaxstr']=data.loc[data['callvol']==data['callvol'].max(),'strike']
-data['cemaxstr']=data.loc[data['calloi']==data['calloi'].max(),'strike']
+rest = {
+    'calloi': [2500, 6500, 7600, 8700, 10000, 6700, 6200, 7700, 5500],
+    'callvol': [6000, 6700, 4300, 6500, 6600, 6700, 15000, 7600, 7800],
+    'strike': [25000, 25050, 25100, 25150, 25200, 25250, 25300, 25350, 25400],
+    'spot': [25187] * 9,
+}
+data = pd.DataFrame(rest)
 
-conds=[(data['spot']<data['cemaxstr'])&(data['spot']<data['volcemaxstr'])&(data['cemaxstr']>data['volcemaxstr']),
-      (data['spot']<data['cemaxstr'])&(data['spot']<data['volcemaxstr'])&(data['cemaxstr']<data['volcemaxstr'])
-      ]
+# FIX: Use .iloc[0] or .max() to get the scalar value instead of a Series
+cemax_strike = data.loc[data['calloi'] == data['calloi'].max(), 'strike'].iloc[0]
+volmax_strike = data.loc[data['callvol'] == data['callvol'].max(), 'strike'].iloc[0]
 
-choice=['Volume is ressistace', 'OI is ressistance']
+# Assigning these to the dataframe for reference
+data['cemaxstr'] = cemax_strike
+data['volcemaxstr'] = volmax_strike
 
-data['status']= np.select(conds,choice, default='strong')
+# Define conditions
+conds = [
+    (data['spot'] < data['cemaxstr']) & (data['spot'] < data['volcemaxstr']) & (data['cemaxstr'] > data['volcemaxstr']),
+    (data['spot'] < data['cemaxstr']) & (data['spot'] < data['volcemaxstr']) & (data['cemaxstr'] < data['volcemaxstr'])
+]
 
+choice = ['Volume is resistance', 'OI is resistance']
 
+# Apply np.select
+data['status'] = np.select(conds, choice, default='No Clear Resistance')
 st.write(data)
