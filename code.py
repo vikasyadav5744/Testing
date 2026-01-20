790ab2c25f10290ec30661a361df1cc7e2c436cd
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-01-20 17:13:57 +0530

    Update code.py

--- code.py
+++ code.py
@@ -462,7 +462,7 @@ with tab3:
             style_df = pd.DataFrame('', index=df.index, columns=df.columns)
             # Set colors only for the 'ce_chang' column
             style_df['CALL_OI'] = np.where(df['ce_chang'] < 0, 'background-color: #ed785a', np.where(df['ce_chang'] > 0, 'background-color: #79a37e', 'background-color: #6f7a71'))
-            style_df['ce_chang'] = np.where(df['ce_chang'] < 0, 'background-color: #ed785a, np.where(df['ce_chang'] > 0, 'background-color: #79a37e', 'background-color: #6f7a71'))
+            style_df['ce_chang'] = np.where(df['ce_chang'] < 0, 'background-color: #ed785a', np.where(df['ce_chang'] > 0, 'background-color: #79a37e', 'background-color: #6f7a71'))
             return style_df
             
         col1, col2, col3=st.columns(3)
