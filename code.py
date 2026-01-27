dc781084a28981e022d3551bcafc1f4641a32b93
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-01-27 17:21:11 +0530

    Update code.py

--- code.py
+++ code.py
@@ -438,13 +438,9 @@ with tab3:
                 st.write(f"""<div style="background-color:#6d8a51; font-size:20px; padding:5px; border-radius: 5px;text-align: center; margin:3px;"> {OIPEVOL_state.volpe_status.iloc[0]}</div>""", unsafe_allow_html=True)
             
             submit= st.button("Filter Date", key='fil1', type='secondary', width='stretch')
-            col1, col2, col3, col4 =st.columns(4)
-            with col1:
-                chart= st.button("OI Charts", key='fil3', type='secondary', width='stretch')
-            with col2:
-                pcr007= st.button("PCR data and Charts", key='fil2', type='secondary', width='stretch')
-            with col3:
-                shifting_change= st.button("Shifting Change data", key='fil4', type='secondary', width='stretch')
+            chart= st.button("OI Charts", key='fil3', type='secondary', width='stretch')
+            pcr007= st.button("PCR data and Charts", key='fil2', type='secondary', width='stretch')
+            shifting_change= st.button("Shifting Change data", key='fil4', type='secondary', width='stretch')
             
             #   play button
             time_option1=newdata.Time.sort_values(ascending=True).unique()
