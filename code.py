04b70da398a407e7271ed1062b57e72d2fecb999
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-01-28 12:27:11 +0530

    Update code.py

--- code.py
+++ code.py
@@ -624,6 +624,16 @@ with tab3:
                         st.dataframe(strike_detail1,hide_index=True, column_order=['Time','ce_chang','CALL_OI','PUT_OI', 'pe_chang', 'ce_intra', 'CALL_CHNG','PUT_CHNG','pe_intra'], height=400) 
                     with col2:
                         st.write("something")
+                        strike_201= st.selectbox("select the begning STRIKE", options=strikes, key='strike201', index=tel6_strike)
+                        strike_detail121 =newdata[newdata['STRIKE']==strike_201][['Time','CALL_OI', 'PUT_OI','CALL_CHNG', 'PUT_CHNG']]
+                        strike_detail121 = strike_detail121.sort_values(by=['Time'])
+                        strike_detail121['ce_chang'] =strike_detail121['CALL_OI'].diff().fillna(0)
+                        strike_detail121['pe_chang'] =strike_detail121['PUT_OI'].diff().fillna(0)
+                        strike_detail121['ce_intra'] =strike_detail121['CALL_CHNG'].diff().fillna(0)
+                        strike_detail121['pe_intra'] =strike_detail121['PUT_CHNG'].diff().fillna(0)
+                        strike_detail121 = strike_detail121.sort_values(by=['Time'], ascending= False)  
+                        strike_detail121= strike_detail121.style.apply(apply_color, axis=None).apply(apply_color1, axis=None).apply(apply_color3, axis=None).apply(apply_color4, axis=None).format(precision=0).format(precision=2, subset=['Time'])
+                        st.dataframe(strike_detail121,hide_index=True, column_order=['Time','ce_chang','CALL_OI','PUT_OI', 'pe_chang', 'ce_intra', 'CALL_CHNG','PUT_CHNG','pe_intra'], height=400)
             if shifting_change ==True:
                 OICE_state =newdata[['ce_status']].drop_duplicates()
                 OIPE_state =newdata[['pe_status']].drop_duplicates()
