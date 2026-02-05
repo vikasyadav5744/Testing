a3eb804f23fae70b623f00a7eebe648d8e1c3c12
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-02-05 10:13:02 +0530

    Update code.py

--- code.py
+++ code.py
@@ -212,7 +212,7 @@ col1, col2= st.columns(2)
 with col1:
     st.write(f"""<div style="background-color:#1686b6; font-size:20px; padding: 5px; border-radius: 5px; text-align: center; margin:3px;color:#77250d;"> Weekly Range One :- {ressis_one}-{support_one}</div> """, unsafe_allow_html=True)
 with col2:
-    st.write(f"""<div style="background-color:#d9bc6c; font-size:20px; padding: 5px; border-radius: 5px;text-align: center; margin:3px, color:green;"> Weekly Range Two:- {ressis_two} - {support_two} </div>""", unsafe_allow_html=True)   
+    st.write(f"""<div style="background-color:#d9bc6c; font-size:20px; padding: 5px; border-radius: 5px;text-align: center; margin:3px, color:#112e16;"> Weekly Range Two:- {ressis_two} - {support_two} </div>""", unsafe_allow_html=True)   
 tab1, tab2, tab3, tab4=st.tabs(["Today's NIFTY", "Addition to Master File", "Historical", "others"])
 with tab1:
     data = st.file_uploader("csv file upload", key='upload1')
